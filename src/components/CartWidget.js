09f7bc427b13102a0c74a3d0fe5bb35a8733a9aa
Jorge <jorgediazok@gmail.com>
2021-08-09 18:27:58 -0300

    Finished styling cart page. Added widget.

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CartContext } from '../context/CartContext';
 
 //STYLES
 import '../styles/CartWidget.css';
 
 const CartWidget = () => {
+  const { cart } = useContext(CartContext);
+
+  console.log(cart);
+
   return (
     <div className='cart-shopping-container'>
       <i className='fas fa-shopping-cart'></i>
-      <span className='badge'>0</span>
+      <span className='badge'>{cart[0].quantity}</span>
     </div>
   );
 };
